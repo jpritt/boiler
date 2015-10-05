eeb0b22e5bd58dff45014c0b0a5204f76113c8d5
Mark Jacob Pritt <jacobpritt@gmail.com>
2015-10-04 20:33:17 -0400

    Space improvements to SAM comparison

--- compareSAMs.py
+++ compareSAMs.py
@@ -44,70 +44,14 @@ def bin(data, width):
         new_data[i] = sum(data[i*width:(i+1)*width])
     return new_data
 
-def compareSAMs(file1, file2):
-    fragment_lengths1 = [0] * 100000
-    reads1 = []
-    countUnpaired = 0
-    countPaired = 0
+def getChromReads(f, chr, fragment_lengths, countUnpaired, countPaired):
+    reads = []
+    for line in f:
+        row = line.rstrip().split('\t')
+        if len(row) < 6:
+            continue
 
-    header = True
-    chromOffsets = dict()
-    covLen = 0
-
-    with open(file1, 'r') as f:
-        for line in f:
-            row = line.rstrip().split('\t')
-            if header and line[0] == '@':
-                if row[0] == '@SQ':
-                    chrom = row[1].split(':')[1]
-                    length = int(row[2].split(':')[1])
-
-                    chromOffsets[chrom] = length
-                    covLen += length
-
-            else:
-                if header:
-                    # End of header
-                    header = False
-                    #cov1 = [0] * covLen
-                    #cov2 = [0] * covLen
-
-                if len(row) < 6:
-                    continue
-
-                if row[6] == '*':
-                    countUnpaired += 1
-                else:
-                    countPaired += 1
-
-                    if row[6] == '=':
-                        l = int(row[8])
-
-                        if l > 0:
-                            if l >= len(fragment_lengths1):
-                                fragment_lengths1 += [0] * (l + 1 - len(fragment_lengths1))
-                            fragment_lengths1[l] += 1
-
-                #exons = parseCigar(row[5], chromOffsets[row[2]]+int(row[3]))
-                #for e in exons:
-                #    for c in range(e[0], e[1]):
-                #        cov1[c] += 1
-
-                read = (row[2], int(row[3]), row[5], row[6], int(row[7]))
-                reads1.append(read)
-    print('%d unpaired, %d paired' % (countUnpaired, countPaired))
-
-    fragment_lengths2 = [0] * len(fragment_lengths1)
-    reads2 = []
-    countUnpaired = 0
-    countPaired = 0
-    with open(file2, 'r') as f:
-        for line in f:
-            row = line.rstrip().split('\t')
-
-            if len(row) < 6:
-                continue
-
+        if row[2] == chr:
             if row[6] == '*':
                 countUnpaired += 1
             else:
@@ -117,19 +61,96 @@ def compareSAMs(file1, file2):
                     l = int(row[8])
 
                     if l > 0:
-                        if l >= len(fragment_lengths2):
-                            fragment_lengths2 += [0] * (l + 1 - len(fragment_lengths2))
-                            fragment_lengths1 += [0] * (l + 1 - len(fragment_lengths1))
-                        fragment_lengths2[l] += 1
+                        if l >= len(fragment_lengths):
+                            fragment_lengths += [0] * (l + 1 - len(fragment_lengths))
+                        fragment_lengths[l] += 1
 
-            exons = parseCigar(row[5], chromOffsets[row[2]]+int(row[3]))
-            #for e in exons:
-            #    for c in range(e[0], e[1]):
-            #        cov2[c] += 1
 
-            read = (row[2], int(row[3]), row[5], row[6], int(row[7]))
-            reads2.append(read)
-    print('%d unpaired, %d paired' % (countUnpaired, countPaired))
+            reads.append((row[2], int(row[3]), row[5], row[6], int(row[7])))
+
+    return reads, countUnpaired, countPaired
+
+def compareSAMs(file1, file2):
+    fragment_lengths1 = [0] * 100000
+    fragment_lengths2 = [0] * 100000
+    countUnpaired1 = 0
+    countPaired1 = 0
+    countUnpaired2 = 0
+    countPaired2 = 0
+
+    chroms = []
+
+    with open(file1, 'r') as f:
+        for line in f:
+            row = line.rstrip().split('\t')
+            if line[0] == '@':
+                if row[0] == '@SQ':
+                    chroms.append(row[1].split(':')[1])
+            else:
+                break
+
+    TP_no_pairs = 0
+    FN_no_pairs = 0
+    FP_no_pairs = 0
+    TP_pairs = 0
+    FN_pairs = 0
+    FP_pairs = 0
+    for c in chroms:
+        with open(file1, 'r') as f:
+            reads1, countUnpaired1, countPaired1 = getChromReads(f, c, fragment_lengths1, countUnpaired1, countPaired1)
+        with open(file2, 'r') as f:
+            reads2, countUnpaired2, countPaired2 = getChromReads(f, c, fragment_lengths2, countUnpaired2, countPaired2)
+
+        reads1.sort()
+        reads2.sort()
+
+        # No pairing
+        tp = 0
+        id = 0
+        for r in reads1:
+            while id < len(reads2) and reads2[id][0] == r[0] and reads2[id][1] < r[1]:
+                id += 1
+
+            if reads2[id][0] == r[0] and reads2[id][1] == r[1] and reads2[id][2] == r[2]:
+                tp += 1
+                id += 1
+
+        TP_no_pairs += tp
+        FN_no_pairs += len(reads1) - tp
+        FP_no_pairs += len(reads2) - tp
+
+        tp = 0
+        id = 0
+        for r in reads1:
+            while id < len(reads2) and reads2[id] < r:
+                id += 1
+
+            if reads2[id] == r:
+                    tp += 1
+                    id += 1
+        TP_pairs += tp
+        FN_pairs += len(reads1) - tp
+        FP_pairs += len(reads2) - tp
+
+    print('No pairing:')
+    print('TP: %d' % TP_no_pairs)
+    print('FN: %d' % FN_no_pairs)
+    print('FP: %d' % FP_no_pairs)
+    print('Recall    = TP / TP+FN = %f' % (float(TP_no_pairs) / (TP_no_pairs+FN_no_pairs)))
+    print('Precision = TP / TP+FP = %f' % (float(TP_no_pairs) / (TP_no_pairs+FP_no_pairs)))
+    print('')
+
+    print('Pairing:')
+    print('TP: %d' % TP_pairs)
+    print('FN: %d' % FN_pairs)
+    print('FP: %d' % FP_pairs)
+    print('Recall    = TP / TP+FN = %f' % (float(TP_pairs) / (TP_pairs+FN_pairs)))
+    print('Precision = TP / TP+FP = %f' % (float(TP_pairs) / (TP_pairs+FP_pairs)))
+    print('')
+
+
+    print('File 1: %d unpaired, %d paired' % (countUnpaired1, countPaired1))
+    print('File 2: %d unpaired, %d paired' % (countUnpaired2, countPaired2))
     print('')
 
 
@@ -170,44 +191,5 @@ def compareSAMs(file1, file2):
     plt.ylabel('Original - Compressed Frequency')
     plt.savefig('frag_len_diff.png')
     plt.clf()
-
-    reads1.sort()
-    reads2.sort()
-
-    print('No pairing:')
-    tp = 0
-    id = 0
-    for r in reads1:
-        while id < len(reads2) and reads2[id][0] == r[0] and reads2[id][1] < r[1]:
-            id += 1
-
-        if reads2[id][0] == r[0] and reads2[id][1] == r[1] and reads2[id][2] == r[2]:
-            tp += 1
-            id += 1
-
-    print('TP: %d' % tp)
-    print('FN: %d' % (len(reads1)-tp))
-    print('FP: %d' % (len(reads2)-tp))
-    print('Recall    = TP / TP+FN = %f' % (float(tp) / len(reads1)))
-    print('Precision = TP / TP+FP = %f' % (float(tp) / len(reads2)))
-    print('')
-
-    print('Pairing:')
-    tp = 0
-    id = 0
-    for r in reads1:
-        while id < len(reads2) and reads2[id] < r:
-            id += 1
-
-        if reads2[id] == r:
-                tp += 1
-                id += 1
-
-    print('TP: %d' % tp)
-    print('FN: %d' % (len(reads1)-tp))
-    print('FP: %d' % (len(reads2)-tp))
-    print('Recall    = TP / TP+FN = %f' % (float(tp) / len(reads1)))
-    print('Precision = TP / TP+FP = %f' % (float(tp) / len(reads2)))
-    print('')
 
 compareSAMs(sys.argv[1], sys.argv[2])
