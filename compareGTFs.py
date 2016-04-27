994d3c35822ecbee7ade999b76339040f4a4c50e
Mark Jacob Pritt <jacobpritt@gmail.com>
2016-04-26 20:52:05 -0400

    Added script to process HISAT input before running Boiler

--- compareGTFs.py
+++ compareGTFs.py
@@ -143,110 +143,12 @@ def compareAll(transcriptsTrue, transcriptsPredicted):
                 totalScore += closestScore
             transcriptsPredictedCount += 1
     precision = float(totalScore) / float(transcriptsPredictedCount)
-    print 'Precision    = TP/P = ' + str(precision)
+    print('Precision    = TP/P = ' + str(precision)
 
-    '''
-    scatterX = []
-    scatterY = []
-    for i in xrange(len(matchingLines)):
-        if matchingLines[i] >= 0:
-            l1 = lines1[i]
-            l2 = lines2[matchingLines[i]]
-            matching += exonsMatch((l1[3], l1[4]), (l2[3], l2[4]), threshold)
-            scatterX += [l1[5]]
-            scatterY += [l2[5]]
 
-    plt.figure(1)
-    plt.scatter(scatterX, scatterY)
-    plt.savefig('scatter.png')
-    '''
 
 weightByCov = False
 if sys.argv[3] == '1':
     weightByCov = True
 compareGTFs(sys.argv[1], sys.argv[2])
 
-
-
-# def compareGTFs(file1, file2):
-#     lines1 = []
-#     with open(file1, 'r') as tsv:
-#         for line in tsv:
-#             row = line.strip().split('\t')
-#             if len(row) >= 5:
-#                 covIndex = row[8].find('cov')
-#                 covStart = row[8].find('"', covIndex) + 1
-#                 covEnd = row[8].find('"', covStart)
-
-#                 lines1.append(row[:3] + [int(row[3])] + [int(row[4])])# + [float(row[8][covStart:covEnd])])
-
-#     lines2 = []
-#     with open(file2, 'r') as tsv:
-#         for line in tsv:
-#             row = line.strip().split('\t')
-#             if len(row) >= 5:
-#                 lines2.append(row[:3] + [int(row[3])] + [int(row[4])])
-
-#     compareAll(lines1, lines2)
-
-# def compareAll(lines1, lines2):
-#     ''' Compare 
-#     '''
-#     numLines1 = len(lines1)
-#     numLines2 = len(lines2)
-    
-#     testedLines1 = 0.0
-#     matching = 0.0
-
-#     #covThreshold = 0
-
-#     print "%d lines in file 1" % numLines1
-#     print "%d lines in file 2" % numLines2
-
-#     index = 0
-#     for l1 in lines1:
-#         index += 1
-#         if index % 1000 == 0:
-#             print "%d / %d lines done" % (index, numLines1)
-
-#         #if l1[5] <= covThreshold:
-#         #    continue
-
-#         testedLines1 += 1
-#         closestScore = 0.0
-        
-#         for l2 in lines2:
-#             if l1[0] == l2[0] and l1[2] == l2[2]:
-#                 if l1[2] == 'transcript':
-#                     threshold = 100
-#                 else:
-#                     threshold = 10
-#                 score = exonsMatch((l1[3], l1[4]), (l2[3], l2[4]), threshold)
-#                 if score > closestScore:
-#                     closestScore = score
-#                     closestMatch = l2
-#         if closestScore > 0:
-#             matching += closestScore
-#             #matching += l1[5]
-
-#     #print str(numTranscripts) + ' transcripts'
-#     print 'Using %d of %d lines' % (testedLines1, numLines1)
-#     print 'TP = ' + str(matching)
-#     print 'T  = ' + str(testedLines1)
-#     #print 'P  = ' + str(numLines2)
-
-#     #print 'Precision = TP/P = ' + str(matching / numLines2)
-#     print 'Recall    = TP/T = ' + str(matching / testedLines1)
-
-
-# def exonsMatch(exon1, exon2, threshold):
-#     ''' Returns a value between 0 and 1, where 1 indicates that the 2 exons are a perfect match and 0 indicates no match
-#     '''
-
-#     dx = min(abs(exon1[0] - exon2[0]), threshold)
-#     dy = min(abs(exon1[1] - exon2[1]), threshold)
-
-#     return 1 - dx / (2.0*threshold) - dy / (2.0*threshold)
-
-
-# compareGTFs(sys.argv[1], sys.argv[2])
